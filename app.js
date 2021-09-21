fbd359cc29d90bd86ed4f7a6c07d1f2cae97eef5
Epic91 <hernandeze791@gmail.com>
2021-09-21 14:46:27 -0500

    created div with nested li tags abd buttons

---
+++ app.js
@@ -0,0 +1,40 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Even Listeners
+    //  adds new todos 
+todoButton.addEventListener('click', addTodo);
+
+// Functions
+function addTodo(event){
+    // prevent form from submitting
+    event.preventDefault();
+    // Create a <div>
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    //  Create <li>
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    // nest <li> inside of <div>
+    todoDiv.appendChild(newTodo);
+
+    // Completed button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML  = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    // Nest <button> inside of <div>
+    todoDiv.appendChild(completedButton);
+
+    // Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML  = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    // Nest <button> inside of <div>
+    todoDiv.appendChild(trashButton);
+    // Nest todoDiv inside of todoList div
+    todoList.appendChild(todoDiv); 
+}
